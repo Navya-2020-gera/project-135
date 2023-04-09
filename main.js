800e4ddeeccaa2485f1a5b55be9a5f6c9d02426b
Navya-2020-gera <83293742+Navya-2020-gera@users.noreply.github.com>
2023-04-09 11:56:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function start(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotposes);
+}
